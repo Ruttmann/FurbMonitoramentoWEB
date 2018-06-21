e1340f10d85a257fbbe46c7ffa043c1366f7f2d9
Ruttmann <m.ruttmann@hotmail.com>
2018-06-21 15:49:35 -0300

    Config. de banco para HEROKU

--- libs/connect_db.js
+++ libs/connect_db.js
@@ -3,7 +3,8 @@ let db
 
 module.exports = function() {
     if (!db) {
-        db = mongoose.connect('mongodb://localhost/arduino')
+        // db = mongoose.connect('mongodb://localhost/arduino')
+        db = mongoose.connect(process.env.MLAB_URI)
         // db.on('error', console.log("Erro de conexão ao banco de dados!"))
     }
     return db
